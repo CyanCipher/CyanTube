eafdc1f1094f70bfb2db2cded18efe360f3ebeb9
CyanCipher <heyitsmemomot@gmail.com>
2023-02-04 02:11:13 +0530

    big big big changes

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+"""
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+"""
+
+
 import os
 import shutil
 import time
@@ -6,6 +34,7 @@ from moviepy.editor import *
 from rich import print
 from rich.console import Console
 from rich.prompt import Prompt
+
 
 logo = """ 
   ____                _____      _
@@ -39,27 +68,25 @@ def clrscr():
 # converter for mp4 -> mp3
 def convert(title, rem=1, cur=1):
     clrscr()
-    console.rule(f"[bold white]Converting: [bold red]{title[0:30]}")
+    console.rule(f"[bold white](Converting: [bold red]{title[0:30]})")
     console.print(f"[{cur}/{rem}]", justify="right")
     wd = os.getcwd()
     video = VideoFileClip(os.path.join(wd, "mp4", f"{title}.mp4"))
     try:
         video.audio.write_audiofile(os.path.join(wd, "mp3", f"{title}.mp3"))
-    except :
+    except:
         print("This file can't be converted into mp3.")
     video.close()
 
 
 def download(url, rem=1, cur=1):
 
-    # working directory
     clrscr()
     wd = os.getcwd()
     folder = "mp4"
     wd = os.path.join(wd, folder)
     yt = YouTube(f'{url}')
-    
-    # setting up title
+
     title = yt.title
     title = title.replace('|', '')
     title = title.replace(',', '')
@@ -68,14 +95,20 @@ def download(url, rem=1, cur=1):
 
     new_title = title[0:30]
     style = "bold white"
-    console.rule(f"[bold white]Downloading: [bold blue]{new_title}")
+    console.rule(f"[bold white](Downloading: [bold blue]{new_title})")
     console.print(f"[{cur}/{rem}]", justify="right")
-    with console.status("Downloading", spinner="simpleDots"):
-        # download and return title
-        stream = yt.streams.get_lowest_resolution()
-        stream.download(filename=(title + ".mp4"), output_path=wd)
-    console.log(f"[bold green]Downloaded file {title}.")
-    return title
+
+    try:
+        with console.status("Downloading", spinner="simpleDots"):
+            # download and return title
+            stream = yt.streams.get_lowest_resolution()
+            stream.download(filename=(title + ".mp4"), output_path=wd)
+            console.log(f"[bold green]Downloaded file {title}.")
+        return title
+
+    except Exception as e:
+        console.log(f"[bold red]Download failed for {title[0:30]}, please try again.")
+        return False
 
 
 # To delete all trash.
@@ -96,37 +129,32 @@ def list_files():
     console.print("[bold blue]These are the downloaded Mp3 files.[/bold blue]")
     for file in files:
         num += 1
-        console.print(f"[bold white]{num} [/bold white] [bold red]- [/bold red] [bold green]{file}[/bold green]")
-
+        console.print(
+            f"[bold white]{num} [/bold white] [bold red]- [/bold red] [bold green]{file}[/bold green]")
 
 
 def main():
-
     i = 1
     clrscr()
     # console log logo
     print(f"[bold cyan]{logo}[/bold cyan]")
-    
+
     video_url = prompt.ask("[bold green]Enter The URL Of PlayList")
-    new_url = video_url.split('?')
-    if new_url[0] == "https://www.youtube.com/playlist":
+    if "playlist" in video_url:
         play_list = Playlist(video_url)
         for url in play_list.video_urls:
-            title = download(url, len(play_list), i)
-            convert(title, len(play_list), i)
+            file_name = download(url, len(play_list), i)
+            if file_name:
+                convert(file_name, len(play_list), i)
             i += 1
     else:
-        title = download(video_url)
-        convert(title)
-    
+        file_name = download(video_url)
+        if file_name:
+            convert(file_name)
+
     delete_files()
     list_files()
 
 
-
 if __name__ == '__main__':
     main()
-    
-
-
-
